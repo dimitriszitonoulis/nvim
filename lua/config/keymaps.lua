5829e732410245f878a8b0a0b0384b3743c29558
dimitriszitonoulis <188760755+dimitriszitonoulis@users.noreply.github.com>
2026-01-22 10:23:23 +0200

    added another keymap for telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -31,6 +31,7 @@ vim.keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "File explorer", silen
 
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader><leader>", ":Telescope live_grep<CR>", { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", { desc = "Telescope help tags" })
 
