2c94e7036d5c0ef4ecb24bee586f6e111e4028d3
dimitriszitonoulis <188760755+dimitriszitonoulis@users.noreply.github.com>
2026-01-31 11:32:23 +0200

    configured custom settings for pyright

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,3 +4,27 @@ vim.lsp.enable({
     "clangd",
     "tinymist",
 })
+
+vim.lsp.config("pyright", {
+    settings = {
+        python = {
+            analysis = {
+                typecheckingmode = "basic",
+                diagnosticSeverityOverrides = {
+                    reportWildcardImportFromLibrary = "none",
+                    reportWildcardImport = "none",
+                },
+            },
+        },
+    },
+})
+
+-- vim.lsp.config("clangd", {
+--     cmd = { "clangd" },
+--     filetypes = { "c", "cpp", "objc", "objcpp" },
+--     root_dir = require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
+--     on_attach = function(client, _)
+--         client.server_capabilities.documentformattingprovider = false
+--         client.server_capabilities.documentrangeformattingprovider = false
+--     end,
+-- })
