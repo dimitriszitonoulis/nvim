b1526b3db46d8842e38fb1402aff2bbf0fa7c1c8
dimitriszitonoulis <188760755+dimitriszitonoulis@users.noreply.github.com>
2026-02-04 22:36:04 +0200

    fixed formatting for django files, added some more options (most are defaults I added them in case I want to change them in the future)

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,5 +3,32 @@
 require("conform").setup({
     formatters_by_ft = {
         python = { "ruff_format" },
-    }
+        html = { "prettierd" },
+        htmldjango = { "prettierd" }
+    },
+    -- Set this to change the default values when calling conform.format()
+    -- This will also affect the default values for format_on_save/format_after_save
+    default_format_opts = {
+        lsp_format = "fallback",
+    },
+    -- Set the log level. Use `:ConformInfo` to see the location of the log file.
+    log_level = vim.log.levels.ERROR,
+    -- Conform will notify you when a formatter errors
+    notify_on_error = true,
+
+
+    -- formatters = {
+    --     prettierd = {
+    --         options = {
+    --             ft_parsers = {
+    --                 html = "html",
+    --                 htmldjango = "htmldjango",
+    --             }
+    --         },
+    --         args = {
+    --             "--tab-width",
+    --             "2"
+    --         },
+    --     }
+    -- },
 })
