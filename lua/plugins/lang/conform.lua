2c557a5e27ad1e765e1c2eab37b0a4c9f9859527
dimitriszitonoulis <188760755+dimitriszitonoulis@users.noreply.github.com>
2026-08-08 11:38:10 +0300

    added prettierd as formatter for yaml

--- lua/plugins/lang/conform.lua
+++ lua/plugins/lang/conform.lua
@@ -11,8 +11,8 @@ require("conform").setup({
         zsh = { "shfmt" },
         c = { "clang-format" },
         cpp = { "clang-format" },
-        markdown = { "prettierd" }
-
+        markdown = { "prettierd" },
+        yaml = { "prettierd" }
     },
     -- Set this to change the default values when calling conform.format()
     -- This will also affect the default values for format_on_save/format_after_save
